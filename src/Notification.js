1be5695da67117e817108f06e7013ad74302712f
sweettirexkiller <piotrjankiewicz02@gmail.com>
2018-03-07 19:34:27 +0100

    testing capitalization

--- src/Notification.js
+++ src/Notification.js
@@ -1,9 +1,14 @@
 export default {
-    template: "<div>{{message}}</div>",
+    template: "<div>{{message | capitalize }}</div>",
     props: ['message'],
     // data(){
     //     return {
     //         message: 'Hello world!'
     //     }
     // }
+    filters: {
+        capitalize(message){
+            return message.toUpperCase();
+        }
+    }
 };
--- test/notification.js
+++ test/notification.js
@@ -15,5 +15,18 @@ test('that it renders a notification', t => {
         }
     }).$mount();
 
-    t.is(vm.$el.textContent, 'foobar')
+    t.is(vm.$el.textContent, 'FOOBAR')
+});
+
+
+test('that is capitalizes the message', t =>{
+    let N = Vue.extend(Notification);
+
+    let vm = new N({
+        propsData: {
+            message: 'foobar'
+        }
+    }).$mount();
+
+    t.is(vm.$el.textContent, 'FOOBAR')
 });
